a32d88ba8062936f33bf061c20976feb5f7f4814
clcrow <6152693+clcrow@users.noreply.github.com>
2020-10-21 21:31:00 -0600

    Solution Accepted

--- Kattis/Program.cs
+++ Kattis/Program.cs
@@ -5,9 +5,45 @@ namespace Kattis
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            //Insert Kattis Solutino Here!
+            Worker workMan = new Worker();
+            workMan.workCode();
+        }
+
+        class Worker
+        {
+            private List<string> inputList = new List<string>();
+            public void workCode()
+            {
+                GetInputs();
+                Console.WriteLine(Calculate());
+            }
+            private string Calculate()
+            {
+                int count;
+                foreach(string word in inputList)
+                {
+                    count = 0;
+                    foreach(string word2 in inputList)
+                    {
+                        if (word == word2) count++;
+                    }
+
+                    if (count > 1) return "no";
+                }
+                return "yes";
+            }
+
+            private void GetInputs()
+            {
+                string input = Console.ReadLine();
+                string[] inputArr = input.Split(' ');
+                foreach (string word in inputArr)
+                {
+                    inputList.Add(word);
+                }
+            }
         }
     }
 }
