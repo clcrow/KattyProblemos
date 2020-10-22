2e6d2dbcb774b7e4c000b953e2de585510196818
clcrow <6152693+clcrow@users.noreply.github.com>
2020-10-21 22:42:00 -0600

    Submission Accepted

--- Kattis/Program.cs
+++ Kattis/Program.cs
@@ -13,20 +13,86 @@ namespace Kattis
 
         class Worker
         {
-            private static string inSize, inWideCut, inLenCut;
-            private double size, wideCut, lenCut, volume;
+            private List<double> walkTimes = new List<double>();
+            private List<double> driveTimes = new List<double>();
+            private List<double> checks = new List<double>();
+            private double start, end, timer, arrivalTime;
+            private bool checker;
             public void workCode()
             {
-                GetInputs();
-                Console.WriteLine(Calculate());
+                GetInputs1();
+                GetInputs2();
+                GetInputs3();
+                GetInputs4();
+                Console.WriteLine(findAnswer(Calculate()));
+            }
+
+            private string findAnswer(double final)
+            {
+                if (final < end) return "yes";
+                else return "no";
             }
             private double Calculate()
             {
-                return 0;
+                for(int i = 0; i < walkTimes.Count; i++)
+                {
+                    start += walkTimes[i];
+                    if (i < driveTimes.Count)
+                    {
+                        while (!checkFunc())
+                        {
+                            start += 1;
+                        }
+                        start += driveTimes[i];
+                    }
+                }
+                return start;
             }
 
-            private void GetInputs()
+
+            private bool checkFunc()
             {
+                checker = false;
+                foreach (double check in checks)
+                {
+                    if (start % check == 0)
+                    {
+                        return true;
+                    }
+                    else checker = false;
+                }
+                return checker;
+            }
+
+            private void GetInputs1()
+            {
+                string[] inputs1 = Console.ReadLine().Split(' ');
+                double.TryParse(inputs1[0], out start);
+                double.TryParse(inputs1[1], out end);
+            }
+            private void GetInputs2()
+            {
+                string[] inputs2 = Console.ReadLine().Split(' ');
+                foreach (string input in inputs2)
+                {
+                    walkTimes.Add(double.Parse(input));
+                }
+            }
+            private void GetInputs3()
+            {
+                string[] inputs3 = Console.ReadLine().Split(' ');
+                foreach (string input in inputs3)
+                {
+                    driveTimes.Add(double.Parse(input));
+                }
+            }
+            private void GetInputs4()
+            {
+                string[] inputs4 = Console.ReadLine().Split(' ');
+                foreach (string input in inputs4)
+                {
+                    checks.Add(double.Parse(input));
+                }
             }
         }
     }
