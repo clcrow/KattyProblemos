db1bb3817abb7d6555c0cd9fe99f152c71ae0566
clcrow <6152693+clcrow@users.noreply.github.com>
2020-10-21 20:36:14 -0600

    Making new base for Kattis Problems

--- Kattis/Program.cs
+++ Kattis/Program.cs
@@ -5,9 +5,29 @@ namespace Kattis
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            //Insert Kattis Solutino Here!
+            Worker workMan = new Worker();
+            workMan.workCode();
+        }
+
+        class Worker
+        {
+            private static string inSize, inWideCut, inLenCut;
+            private double size, wideCut, lenCut, volume;
+            public void workCode()
+            {
+                GetInputs();
+                Console.WriteLine(Calculate());
+            }
+            private double Calculate()
+            {
+                return 0;
+            }
+
+            private void GetInputs()
+            {
+            }
         }
     }
 }
