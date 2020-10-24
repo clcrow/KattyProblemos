47fddc2da5107bb6a8dab33af57eca71e08fb4bc
clcrow <6152693+clcrow@users.noreply.github.com>
2020-10-23 23:50:49 -0600

    Submission Accepted

--- Kattis/Program.cs
+++ Kattis/Program.cs
@@ -13,18 +13,40 @@ namespace Kattis
 
         class Worker
         {
+            private string[] monies;
+            private long expenseTotal, expenseCount;
             public void workCode()
             {
                 GetInputs();
-                Console.WriteLine(Calculate());
+                if (!allPositive())
+                {
+                    Console.WriteLine(Calculate());
+                }
+                else Console.WriteLine('0');
             }
-            private double Calculate()
+
+            private bool allPositive()
             {
-                return 0;
+                for (int i = 0; i < expenseCount; i++)
+                {
+                    if (0 >= double.Parse(monies[i])) return false;
+                }
+                return true;
+            }
+            private long Calculate()
+            {
+                expenseTotal = 0;
+                foreach (string money in monies)
+                {
+                    if (long.Parse(money) <= 0) expenseTotal += long.Parse(money);
+                }
+                return 0 - expenseTotal;
             }
 
             private void GetInputs()
             {
+                expenseCount = long.Parse(Console.ReadLine());
+                monies = Console.ReadLine().Split(' ');
             }
         }
     }
