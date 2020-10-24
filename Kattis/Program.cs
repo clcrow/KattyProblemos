cb7d861791500d358a8a430af9843c26554b7656
clcrow <6152693+clcrow@users.noreply.github.com>
2020-10-24 00:12:01 -0600

    Submission Accepted

--- Kattis/Program.cs
+++ Kattis/Program.cs
@@ -13,18 +13,55 @@ namespace Kattis
 
         class Worker
         {
+            private long lineCount;
+            private List<string> schools = new List<string>();
+            private List<string> teams = new List<string>();
+            private List<string> finalSchools = new List<string>();
+            private List<string> finalTeams = new List<string>();
             public void workCode()
             {
+                GetLineNum();
                 GetInputs();
-                Console.WriteLine(Calculate());
+                Calculate();
+                Output();
             }
-            private double Calculate()
+
+            private void Output()
             {
-                return 0;
+                for (int i = 0; i < 12; i++)
+                {
+                    Console.WriteLine(finalSchools[i] + " " + finalTeams[i]);
+                }
+            }
+            private void Calculate()
+            {
+                int i = 0;
+                int j = 0;
+                while (i < 12)
+                {
+                    if(!finalSchools.Contains(schools[j]))
+                    {
+                        finalSchools.Add(schools[j]);
+                        finalTeams.Add(teams[j]);
+                        i++;
+                    }
+                    j++;
+                }
+            }
+
+            private void GetLineNum()
+            {
+                long.TryParse(Console.ReadLine(), out lineCount);
             }
 
             private void GetInputs()
             {
+                for (int i = 0; i < lineCount; i++)
+                {
+                    string[] input = Console.ReadLine().Split(' ');
+                    schools.Add(input[0]);
+                    teams.Add(input[1]);
+                }
             }
         }
     }
