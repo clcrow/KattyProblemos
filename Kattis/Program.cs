6c7e8595e232e76f70cbcb1eeb93a3139ca037ac
clcrow <6152693+clcrow@users.noreply.github.com>
2020-10-30 22:45:07 -0600

    Submission Accepted

--- Kattis/Program.cs
+++ Kattis/Program.cs
@@ -13,6 +13,8 @@ namespace Kattis
 
         class Worker
         {
+            private double numSum;
+            private string startNum, num;
             public void workCode()
             {
                 GetInputs();
@@ -20,11 +22,25 @@ namespace Kattis
             }
             private double Calculate()
             {
-                return 0;
+                num = startNum;
+                for(int j = int.Parse(startNum); true; j++)
+                {
+                    numSum = 0;
+                    for (int i = 0; i < num.Length; i++)
+                    {
+                        numSum += double.Parse(num.Substring(i, 1));
+                    }
+                    if (j % numSum == 0)
+                    {
+                        return j;
+                    }
+                    num = (int.Parse(num) + 1).ToString();
+                }
             }
 
             private void GetInputs()
             {
+                startNum = Console.ReadLine();
             }
         }
     }
