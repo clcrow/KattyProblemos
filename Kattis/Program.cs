f26164bc49017d8c769814c01cef3b07ace1614d
clcrow <6152693+clcrow@users.noreply.github.com>
2021-01-01 16:09:08 -0700

    Submission Accepted

--- Kattis/Program.cs
+++ Kattis/Program.cs
@@ -13,18 +13,33 @@ namespace Kattis
 
         class Worker
         {
+            public string input;
+            public string answer;
             public void workCode()
             {
                 GetInputs();
                 Console.WriteLine(Calculate());
             }
-            private double Calculate()
+            private string Calculate()
             {
-                return 0;
+                answer = "";
+
+                char[] inputarr = input.ToCharArray();
+                for (int i = 0; i < inputarr.Length; i++)
+                {
+                    if ((inputarr[i] == 'a' || inputarr[i] == 'e') || (inputarr[i] == 'i' || inputarr[i] == 'o') || inputarr[i] == 'u')
+                    {
+                        answer += inputarr[i];
+                        i += 2;
+                    }
+                    else answer += inputarr[i];
+                }
+                return answer;
             }
 
             private void GetInputs()
             {
+                input = Console.ReadLine();
             }
         }
     }
