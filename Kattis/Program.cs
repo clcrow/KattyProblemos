aec73bde3ef3fe501d57fce8bc838183b4af9e0e
clcrow <6152693+clcrow@users.noreply.github.com>
2021-01-01 13:14:57 -0700

    Submission Accepted

--- Kattis/Program.cs
+++ Kattis/Program.cs
@@ -13,8 +13,9 @@ namespace Kattis
 
         class Worker
         {
-            private double cases;
-            private List<double> outlets = new List<double>();
+            private double cases, tally;
+            private int outNum;
+            private List<int> outlets = new List<int>();
             public void workCode()
             {
                 GetInputs();
@@ -25,8 +26,20 @@ namespace Kattis
             }
             private double Calculate()
             {
-                outl
-                return 0;
+                tally = 0;
+                string[] arr = Console.ReadLine().Split(' ');
+                foreach (string item in arr)
+                {
+                    outlets.Add(int.Parse(item));
+                }
+                outNum = outlets[0];
+                for(int i = 0; i < outNum - 1; i++)
+                {
+                    tally += outlets[i + 1] - 1;
+                }
+                tally += outlets[outNum];
+                outlets.Clear();
+                return tally;
             }
 
             private void GetInputs()
