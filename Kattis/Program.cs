1ee3c9997762548677e470e144fb0bebc78cbc1b
clcrow <6152693+clcrow@users.noreply.github.com>
2020-12-31 17:02:22 -0700

    Submission Accepted

--- Kattis/Program.cs
+++ Kattis/Program.cs
@@ -13,6 +13,8 @@ namespace Kattis
 
         class Worker
         {
+            List<char> answers = new List<char>();
+            double questCount, rightAns;
             public void workCode()
             {
                 GetInputs();
@@ -20,11 +22,25 @@ namespace Kattis
             }
             private double Calculate()
             {
-                return 0;
+                rightAns = 0;
+                int count = answers.Count;
+                for (int i = 0; i < count - 1; i++)
+                {
+                    if (answers[i] == answers[i + 1])
+                    {
+                        rightAns++;
+                    }
+                }
+                return rightAns;
             }
 
             private void GetInputs()
             {
+                questCount = double.Parse(Console.ReadLine());
+                for (int i = 0; i < questCount; i++)
+                {
+                    answers.Add(char.Parse(Console.ReadLine()));
+                }
             }
         }
     }
