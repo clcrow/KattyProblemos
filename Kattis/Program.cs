97ee7cbc5dbb0b7041ac597498b41a71c46cd05d
clcrow <6152693+clcrow@users.noreply.github.com>
2021-01-01 13:49:21 -0700

    Submission Accepted

--- Kattis/Program.cs
+++ Kattis/Program.cs
@@ -13,18 +13,35 @@ namespace Kattis
 
         class Worker
         {
+            public int len;
+            public List<int> input = new List<int>();
+            public List<int> output = new List<int>();
+            public List<int> answer = new List<int>();
             public void workCode()
             {
                 GetInputs();
-                Console.WriteLine(Calculate());
+                answer = Calculate();
+                foreach (int item in answer)
+                {
+                    Console.WriteLine(item);
+                }
             }
-            private double Calculate()
+            private List<int> Calculate()
             {
-                return 0;
+                for (int i = 0; i < len; i++)
+                {
+                    input.Add(int.Parse(Console.ReadLine()));
+                }
+                for (int j = len-1; j >= 0; j--)
+                {
+                    output.Add(input[j]);
+                }
+                return output;
             }
 
             private void GetInputs()
             {
+                len = int.Parse(Console.ReadLine());
             }
         }
     }
