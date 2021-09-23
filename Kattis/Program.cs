f2b8af93e6708d2c533bf422df7791f2fa14f2b9
clcrow <6152693+clcrow@users.noreply.github.com>
2021-09-23 13:01:32 -0600

    Submission Accepted

--- Kattis/Program.cs
+++ Kattis/Program.cs
@@ -13,18 +13,37 @@ namespace Kattis
 
         class Worker
         {
+            int count;
+            List<int> inputs = new List<int>();
+            int intResult = 0;
             public void workCode()
             {
                 GetInputs();
                 Console.WriteLine(Calculate());
             }
-            private double Calculate()
+            private string Calculate()
             {
-                return 0;
+                if(count % 2 == 1)
+                {
+                    return "still running";
+                }
+                else
+                {
+                    for(int i = 0; i < count / 2; i++)
+                    {
+                        intResult += inputs[(i * 2) + 1] - inputs[i * 2];
+                    }
+                    return intResult.ToString();
+                }
             }
 
             private void GetInputs()
             {
+                count = int.Parse(Console.ReadLine());
+                for (int i = 0; i < count; i++)
+                {
+                    inputs.Add(int.Parse(Console.ReadLine()));
+                }
             }
         }
     }
