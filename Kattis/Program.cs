594a48f4c5620257e0bc27d7e0313a3574ec4186
clcrow <6152693+clcrow@users.noreply.github.com>
2021-09-23 12:03:08 -0600

    Submission Accepted

--- Kattis/Program.cs
+++ Kattis/Program.cs
@@ -13,18 +13,40 @@ namespace Kattis
 
         class Worker
         {
+            double[] judgeLine = new double[2];
+            List<double> grades = new List<double>();
             public void workCode()
             {
                 GetInputs();
                 Console.WriteLine(Calculate());
             }
-            private double Calculate()
+            private string Calculate()
             {
-                return 0;
+                double[] results = new double[2];
+                string strResults = "";
+                double leftOver = judgeLine[0] - judgeLine[1];
+                double currentScore = 0;
+                for(int i = 0; i < judgeLine[1]; i++)
+                {
+                    currentScore += grades[i];
+                }
+                results[0] = (currentScore + (leftOver * -3)) / judgeLine[0];
+                results[1] = (currentScore + (leftOver * 3)) / judgeLine[0];
+                strResults = results[0].ToString() + ' ' + results[1].ToString();
+                return strResults;
             }
 
             private void GetInputs()
             {
+                string[] strJudgeLine = Console.ReadLine().Split(' ');
+                for (int i = 0; i < 2; i++)
+                {
+                    judgeLine[i] = double.Parse(strJudgeLine[i]);
+                }
+                for (int i = 0; i < judgeLine[1]; i++)
+                {
+                    grades.Add(double.Parse(Console.ReadLine()));
+                }
             }
         }
     }
