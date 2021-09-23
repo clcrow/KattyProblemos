bffcef5c9ad734527b427602bd54670ed4cba5ad
clcrow <6152693+clcrow@users.noreply.github.com>
2021-09-23 13:10:18 -0600

    Submission Accepted

--- Kattis/Program.cs
+++ Kattis/Program.cs
@@ -13,18 +13,39 @@ namespace Kattis
 
         class Worker
         {
+            int count;
+            List<string> inputs = new List<string>();
             public void workCode()
             {
                 GetInputs();
                 Console.WriteLine(Calculate());
             }
-            private double Calculate()
+            private int Calculate()
             {
-                return 0;
+                int result = 0;
+                for (int i = 0; i < count-1; i++)
+                {
+                    int timeOne = int.Parse(inputs[i].Split(' ')[0]);
+                    int speedOne = int.Parse(inputs[i].Split(' ')[1]);
+                    int timeTwo = int.Parse(inputs[i + 1].Split(' ')[0]);
+                    int speedTwo = int.Parse(inputs[i + 1].Split(' ')[1]);
+
+                    int newResult = (speedTwo - speedOne) / (timeTwo - timeOne);
+                    if(newResult > result)
+                    {
+                        result = newResult;
+                    }
+                }
+                return result;
             }
 
             private void GetInputs()
             {
+                count = int.Parse(Console.ReadLine());
+                for (int i = 0; i < count; i++)
+                {
+                    inputs.Add(Console.ReadLine());
+                }
             }
         }
     }
