9eb9fb5a3b099cc613e3ede6d6000d1be204dd6f
clcrow <6152693+clcrow@users.noreply.github.com>
2021-09-30 11:50:58 -0600

    Submission Accepted

--- Kattis/Program.cs
+++ Kattis/Program.cs
@@ -13,18 +13,43 @@ namespace Kattis
 
         class Worker
         {
+            string[] firstInputs = new string[2];
+            int buildCount, requestCount;
+            List<int> answers = new List<int>();
             public void workCode()
             {
                 GetInputs();
-                Console.WriteLine(Calculate());
+                string[] buildLocs = new string[buildCount];
+                buildLocs = Console.ReadLine().Split(' ');
+                for (int i = 0; i < requestCount; i++)
+                {
+                    Calculate(buildLocs);
+                }
+                foreach(int i in answers)
+                {
+                    Console.WriteLine(i);
+                }
             }
-            private double Calculate()
+            private void Calculate(string[] buildLocs)
             {
-                return 0;
+                string[] request = new string[3];
+                request = Console.ReadLine().Split(' ');
+                if(request[0] == "1")
+                {
+                    buildLocs[int.Parse(request[1]) - 1] = request[2];
+                }
+                else if(request[0] == "2")
+                {
+                    int distance = int.Parse(buildLocs[int.Parse(request[1]) - 1]) - int.Parse(buildLocs[int.Parse(request[2]) - 1]);
+                    answers.Add(Math.Abs(distance));
+                }
             }
 
             private void GetInputs()
             {
+                firstInputs = Console.ReadLine().Split(' ');
+                buildCount = int.Parse(firstInputs[0]);
+                requestCount = int.Parse(firstInputs[1]);
             }
         }
     }
