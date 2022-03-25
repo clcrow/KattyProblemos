c4d404a154e975b3c6d3650be07c54bd172d713b
clcrow <6152693+clcrow@users.noreply.github.com>
2022-03-25 12:57:26 -0600

    Submission Accepted

--- Kattis/Program.cs
+++ Kattis/Program.cs
@@ -13,18 +13,28 @@ namespace Kattis
 
         class Worker
         {
+            private string input;
             public void workCode()
             {
                 GetInputs();
                 Console.WriteLine(Calculate());
             }
-            private double Calculate()
+            private string Calculate()
             {
-                return 0;
+                int index = input.IndexOf("()");
+                string firstPart = input.Substring(0, index);
+                string secondPart = input.Substring(index + 2);
+                if(firstPart == secondPart)
+                {
+                    return "correct";
+                }
+
+                else return "fix";
             }
 
             private void GetInputs()
             {
+                input = Console.ReadLine();
             }
         }
     }
